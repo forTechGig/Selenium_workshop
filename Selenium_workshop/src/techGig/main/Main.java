2d7adec7dbdb1ebed60758a0a8df2bc8f27befe2
Raja <techGig@gmail.com>
2017-04-15 18:20:51 -0400

    SetUp

---
+++ Selenium_workshop/src/techGig/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package techGig.main;
+
+/**
+ * @author SXP8101
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
